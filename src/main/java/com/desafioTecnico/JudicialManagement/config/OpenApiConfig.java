d62f3d0aa9633e6688e043f3d8229b12143ec416
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-06-25 18:20:47 -0300

    feat: Create OpenApiConfig

--- src/main/java/com/desafioTecnico/JudicialManagement/config/OpenApiConfig.java
+++ src/main/java/com/desafioTecnico/JudicialManagement/config/OpenApiConfig.java
@@ -1,4 +1,17 @@
 package com.desafioTecnico.JudicialManagement.config;
 
-public class OpenApiConfig {
-}
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API de Gestão de Processos", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {}
