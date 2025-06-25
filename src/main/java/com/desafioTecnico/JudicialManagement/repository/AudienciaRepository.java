071a8e7511167aa8a6771c8986cb3ca5cf830ee5
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-06-25 16:18:49 -0300

    feat: Creates repositories

--- src/main/java/com/desafioTecnico/JudicialManagement/repository/AudienciaRepository.java
+++ src/main/java/com/desafioTecnico/JudicialManagement/repository/AudienciaRepository.java
@@ -1,4 +1,18 @@
 package com.desafioTecnico.JudicialManagement.repository;
 
-public interface AudienciaRepository {
+import com.desafioTecnico.JudicialManagement.model.Audiencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AudienciaRepository extends JpaRepository<Audiencia, Long> {
+
+    @Query("SELECT COUNT(a) > 0 FROM Audiencia a WHERE a.processo.vara = :vara AND a.local = :local AND a.dataHora = :dataHora")
+    boolean existsByVaraAndLocalAndDataHora(String vara, String local, LocalDateTime dataHora);
+
+    @Query("SELECT a FROM Audiencia a WHERE a.processo.comarca = :comarca AND a.dataHora >= :inicioDoDia AND a.dataHora <= :fimDoDia")
+    List<Audiencia> findByComarcaAndData(String comarca, LocalDateTime inicioDoDia, LocalDateTime fimDoDia);
 }
--- src/main/java/com/desafioTecnico/JudicialManagement/repository/ProcessoRepository.java
+++ src/main/java/com/desafioTecnico/JudicialManagement/repository/ProcessoRepository.java
@@ -1,4 +1,16 @@
 package com.desafioTecnico.JudicialManagement.repository;
 
-public interface ProcessoRepository {
+import com.desafioTecnico.JudicialManagement.model.Processo;
+import com.desafioTecnico.JudicialManagement.model.StatusProcesso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProcessoRepository extends JpaRepository<Processo, Long> {
+
+    List<Processo> findByStatus(StatusProcesso status);
+    List<Processo> findByComarca(String comarca);
+    List<Processo> findByStatusAndComarca(StatusProcesso status, String comarca);
 }
