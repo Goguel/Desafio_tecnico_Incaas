d528da5be15ffd1b0188bba8b0be89a01b951658
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-06-25 15:54:08 -0300

    Initial commit

---
+++ src/main/java/com/desafioTecnico/JudicialManagement/JudicialManagementApplication.java
@@ -0,0 +1,13 @@
+package com.desafioTecnico.JudicialManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudicialManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudicialManagementApplication.class, args);
+	}
+
+}
