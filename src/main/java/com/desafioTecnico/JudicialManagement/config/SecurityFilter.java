248f271bef7dc93016d945008efc978e03e2846b
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-06-25 17:36:38 -0300

    feat: Creates SecurityFilter

---
+++ src/main/java/com/desafioTecnico/JudicialManagement/config/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.desafioTecnico.JudicialManagement.config;
+
+import com.desafioTecnico.JudicialManagement.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recuperarToken(request);
+        if (token != null) {
+            String username = tokenService.getUsernameFromToken(token);
+
+            UserDetails user = new User(username, "", Collections.emptyList());
+
+            // Informa ao Spring que o usuário está autenticado para esta requisição
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authHeader.substring(7); // Remove "Bearer "
+    }
+}
